1bc19c5328099fa57680d471479aec40e8cd2c0f
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-12-02 08:54:54 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # make sure directories exists
-mkdir cmake
-mkdir src
-mkdir input
+mkdir -p cmake
+mkdir -p src
+mkdir -p input
 
 # Add executable to CMakeLists.txt
 cat <<EOT > cmake/day_$1.cmake
